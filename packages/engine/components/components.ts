fff95d3a72eb4fbcafeeedd6f80fd58f268510e0
zhangxiaobin <silence.tiger@qq.com>
2025-03-29 00:40:13 +0800

    feat: update export interface

--- packages/engine/components/components.ts
+++ packages/engine/components/components.ts
@@ -1,9 +1,14 @@
 export * from './Bubble';
+export * from './Bubble/interface';
 
 export * from './BubbleText';
+export * from './BubbleText/interface';
 
 export * from './Sender';
+export * from './Sender/interface';
 
 export * from './Skill';
+export * from './Skill/interface';
 
 export * from './Layout';
+export * from './Layout/interface';
