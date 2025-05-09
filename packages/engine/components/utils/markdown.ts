192ee9387ef570605c9a4e0a60a547dd82e18c39
zhangxiaobin <silence.tiger@qq.com>
2025-05-09 08:45:26 +0800

    feat: update md style

--- packages/engine/components/utils/markdown.ts
+++ packages/engine/components/utils/markdown.ts
@@ -2,9 +2,9 @@ import MarkdownIt from 'markdown-it';
 import hljs from 'highlight.js';
 import markdownItHighlight from 'markdown-it-highlightjs';
 
-import 'highlight.js/styles/base16/github.min.css';
+import 'highlight.js/styles/base16/default-light.css';
 
-const md = MarkdownIt({ html: true, breaks: true });
+const md = MarkdownIt({ html: false, breaks: true });
 
 md.use(markdownItHighlight, { hljs });
 
